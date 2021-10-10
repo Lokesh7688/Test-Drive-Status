0c920441a23842585070f80f98601b321680573a
Lokesh@007688 <lokesh0kashyap@gmail.com>
2021-10-10 14:17:55 +0530

    error_fixed

--- Models.py
+++ Models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
  
-db = SQLAlchemy()
+db =SQLAlchemy()
  
 class CustomerModel(db.Model):
     __tablename__ = "table"
@@ -10,12 +10,10 @@ class CustomerModel(db.Model):
     name = db.Column(db.String())
     age = db.Column(db.Integer())
     status = db.Column(db.String(80))
+    
  
-    def __init__(self, employee_id,name,age,status):
+    def __init__(self, customer_id,name,age,status):
         self.customer_id = customer_id
         self.name = name
         self.age = age
         self.status = status
- 
-    def __repr__(self):
-        return f"{self.name}:{self.customer_id}"
--- main.py
+++ main.py
@@ -1,81 +1,75 @@
 from flask import Flask,render_template,request,redirect
-from Models import db,CustomerModel
-
+from models import db,CustomerModel
+ 
 app = Flask(__name__)
+ 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-
+ 
 @app.before_first_request
 def create_table():
     db.create_all()
-
-
-@app.route('/data/create', methods=['GET', 'POST'])
+ 
+@app.route('/create' , methods = ['GET','POST'])
 def create():
     if request.method == 'GET':
         return render_template('createpage.html')
-
+ 
     if request.method == 'POST':
         customer_id = request.form['customer_id']
         name = request.form['name']
         age = request.form['age']
         status = request.form['status']
-        customer = CustomerModel(
-            customer_id=customer_id, name=name, age=age, status=status)
+        customer = CustomerModel(customer_id=customer_id, name=name, age=age, status = status)
         db.session.add(customer)
         db.session.commit()
-        return redirect('/data')
-
-
-@app.route('/data')
-def RetrieveDataList():
-    customer = CustomerModel.query.all()
-    return render_template('datalist.html', customer=customer)
-
-
-@app.route('/data/<int:id>')
-def RetrieveSinglecustomer(id):
+        return redirect('/')
+ 
+ 
+@app.route('/')
+def RetrieveList():
+    customers = CustomerModel.query.all()
+    return render_template('index.html',customers = customers)
+ 
+ 
+@app.route('/<int:id>')
+def Retrievecustomer(id):
     customer = CustomerModel.query.filter_by(customer_id=id).first()
     if customer:
-        return render_template('data.html', customer=customer)
+        return render_template('data.html', customer = customer)
     return f"customer with id ={id} Doenst exist"
-
-
-@app.route('/data/<int:id>/update', methods=['GET', 'POST'])
+ 
+ 
+@app.route('/<int:id>/update',methods = ['GET','POST'])
 def update(id):
     customer = CustomerModel.query.filter_by(customer_id=id).first()
     if request.method == 'POST':
         if customer:
             db.session.delete(customer)
             db.session.commit()
-
             name = request.form['name']
             age = request.form['age']
             status = request.form['status']
-            customer = CustomerModel(
-                customer_id=id, name=name, age=age, status=status)
-
+            customer = CustomerModel(customer_id=id, name=name, age=age, status = status)
             db.session.add(customer)
             db.session.commit()
-            return redirect(f'/data/{id}')
+            return redirect(f'/{id}')
         return f"customer with id = {id} Does nit exist"
-
-    return render_template('update.html', customer=customer)
-
-
-@app.route('/data/<int:id>/delete', methods=['GET', 'POST'])
+ 
+    return render_template('update.html', customer = customer)
+ 
+ 
+@app.route('/<int:id>/delete', methods=['GET','POST'])
 def delete(id):
     customer = CustomerModel.query.filter_by(customer_id=id).first()
     if request.method == 'POST':
         if customer:
             db.session.delete(customer)
             db.session.commit()
-            return redirect('/data')
+            return redirect('/')
         abort(404)
-
+ 
     return render_template('delete.html')
-
-
-if __name__ == '__main__':
-    app.run()
+ 
+app.run(host='localhost', port=5000)
